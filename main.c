77dbd97b06d4a452b245248f3a61b313df66e0ac
thddlrla <okjh0999@naver.com>
2023-10-13 16:36:59 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	void func1(void)
+	{
+		int x;
+		printf("func1 x is at %p\n" ,&x);		
+	}
+
+int main(int argc, char *argv[]) {
+	
+	int main(void)
+	{
+		int x;
+		printf("main x is at %p\n", &x);
+		func();
+		
+}
+	return 0;
+
+} 
